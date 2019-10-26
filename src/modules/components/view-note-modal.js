c8d64eaf4324371d7102f0a51715a9050899fd46
Shady Noah <snm@mimonote.com>
2019-10-26 16:48:26 +0200

    enhance view note modal for android

--- src/modules/components/view-note-modal.js
+++ src/modules/components/view-note-modal.js
@@ -1,59 +1,108 @@
-import React, { PureComponent, Fragment } from 'react';
-import { ScrollView, Dimensions, Image, WebView } from 'react-native';
-import { Container, Header, Text, Content, Card, CardItem, Body, ListItem, Left, Right, Button, Icon, View } from 'native-base';
-import Modal from 'react-native-modal';
-
+import React, { PureComponent, Fragment } from "react";
+import { ScrollView, Dimensions, Image, WebView } from "react-native";
+import {
+  Container,
+  Header,
+  Text,
+  Content,
+  Card,
+  CardItem,
+  Body,
+  ListItem,
+  Left,
+  Right,
+  Button,
+  Icon,
+  View
+} from "native-base";
+import Modal from "react-native-modal";
 
 import _ from "lodash";
 // var QuillDeltaToHtmlConverter = require('quill-delta-to-html').QuillDeltaToHtmlConverter;
 // import { QuillDeltaToHtmlConverter } from '../../../quill-delta-to-html/dist/commonjs/main';
-const SUPPORTED_ORIENTATIONS = ['portrait', 'portrait-upside-down', 'landscape', 'landscape-left', 'landscape-right'];
+const SUPPORTED_ORIENTATIONS = [
+  "portrait",
+  "portrait-upside-down",
+  "landscape",
+  "landscape-left",
+  "landscape-right"
+];
 
 export class ViewNoteModal extends PureComponent {
+  props: {
+    onToggleViewNoteModal: () => void,
+    isViewModal: boolean,
+    selectedNote: Note,
+    selectedTemplate: Template
+  };
 
-    props: {
-        onToggleViewNoteModal: () => void,
-        isViewModal: boolean,
-        selectedNote: Note,
-        selectedTemplate: Template
-    }
-
-    render() {
-        return (
-            <Modal
-                animationType="slide"
-                transparent={false}
-                isVisible={this.props.isViewModal}
-                supportedOrientations={SUPPORTED_ORIENTATIONS}
-               
-            >
-                <Container style={{ flex: 1 }}>
-                    <Header defaultModal>
-                        <Left />
-                        <Body><Text modalHeaderTitle>View Note </Text></Body>
-                        <Right>
-                            <Icon black name="close" onPress={() => this.props.toggleViewNoteModal()} />
-                        </Right>
-                    </Header>
-                    <Content modalContent padder={true}>
-                        <View style={{ flex: 1, flexDirection: 'row', justifyContent: 'center' }}>
-                        
-                                <Text>
-                                {this.props.selectedNote.bookName} {this.props.selectedNote.chapterNumber}
-                                </Text>
-                                
-                        
-                          
-                        </View>
+  render() {
+      const defaultModal = {
+        backgroundColor: "#ecf0f1",
+        justifyContent: "space-between",
+        borderBottomColor: "#ecf0f1"
+      }
+      const  modalHeaderTitle = {
+        fontSize: 20,
+      fontWeight: "bold",
+      alignSelf: "center"
+      }
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        isVisible={this.props.isViewModal}
+        supportedOrientations={SUPPORTED_ORIENTATIONS}
+      >
+        <Container style={{ flex: 1 }}>
+          <Header
+            style={{
+              backgroundColor: "#ecf0f1",
+              justifyContent: 'flex-start',
+              borderBottomColor: "#ecf0f1"
+            }}
+          >
+            <Left />
+            <Body>
+              <Text
+                style={{
+                    textAlign: 'center', // <-- the magic
+                    fontWeight: 'bold',
+                    fontSize: 18,
+                    marginTop: 0,
+                    width: 200
                    
-                        <Text>
-                     {this.props.selectedNote.versesText}
-                        </Text>
-                        
-                    </Content>
-                </Container>
-            </Modal>
+                }}
+              >
+                View Note{" "}
+              </Text>
+            </Body>
+            <Right>
+              <Icon
+                black
+                name="close"
+                onPress={() => this.props.toggleViewNoteModal()}
+              />
+            </Right>
+          </Header>
+          <Content modalContent padder={true}>
+            <View
+              style={{
+                flex: 1,
+                flexDirection: "row",
+                justifyContent: "center"
+              }}
+            >
+              <Text>
+                {this.props.selectedNote.bookName}{" "}
+                {this.props.selectedNote.chapterNumber}
+              </Text>
+            </View>
 
-        );
-    }
+            <Text>{this.props.selectedNote.versesText}</Text>
+          </Content>
+        </Container>
+      </Modal>
+    );
+  }
 }
